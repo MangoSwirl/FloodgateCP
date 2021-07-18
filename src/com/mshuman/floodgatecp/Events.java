e71c00ac8f800c103df20a5d1cb3205f8db02a24
MangoSwirl <collin.camerono@gmail.com>
2021-07-18 13:30:54 -0700

    I really should commit more often, that makes writing these messages much easier.

--- src/com/mshuman/floodgatecp/Events.java
+++ src/com/mshuman/floodgatecp/Events.java
@@ -1,16 +1,39 @@
 package com.mshuman.floodgatecp;
 
 import me.rockyhawk.commandpanels.api.PanelOpenedEvent;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.geysermc.cumulus.SimpleForm;
+import org.geysermc.cumulus.response.SimpleFormResponse;
 import org.geysermc.floodgate.api.FloodgateApi;
+import org.geysermc.floodgate.api.player.FloodgatePlayer;
+
 
 public class Events implements Listener {
     @EventHandler
     public void onPanelOpen(PanelOpenedEvent event) {
         if (!FloodgateApi.getInstance().isFloodgatePlayer(event.getPlayer().getUniqueId())) return;
 
-        event.getPlayer().sendMessage(FloodgateCP.getInstance().getConfig().getString("lang.prefix") + FloodgateCP.getInstance().getConfig().getString("lang.panel-not-available"));
+        if (FloodgateCP.getInstance().getConfig().getConfigurationSection("panels").contains(event.getPanel().getName())) {
+            FloodgatePlayer fgPlayer = FloodgateApi.getInstance().getPlayer(event.getPlayer().getUniqueId());
+            ConfigurationSection fgPanel = Util.getConfig().getConfigurationSection("panels." + event.getPanel().getName());
+            SimpleForm.Builder form = SimpleForm.builder();
+            form = form.title(fgPanel.getString("title"));
+            form = form.content(fgPanel.getString("content"));
+
+            for (String i : fgPanel.getConfigurationSection("buttons").getKeys(false)) {
+                form = form.button(fgPanel.getString("buttons." + i + ".content"));
+            }
+            form = form.responseHandler((SimpleForm returnedForm, String rawResponse) -> {
+                SimpleFormResponse response = returnedForm.parseResponse(rawResponse);
+                if (!response.isCorrect()) return;
+                Util.handleResponse(event.getPanel().getName(), response, returnedForm, event.getPlayer());
+            });
+            fgPlayer.sendForm(form.build());
+        } else {
+            event.getPlayer().sendMessage(Util.getLang("prefix") + Util.getLang("panel-not-available"));
+        }
         event.setCancelled(true);
     }
 }
---
+++ src/com/mshuman/floodgatecp/Util.java
@@ -0,0 +1,30 @@
+package com.mshuman.floodgatecp;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.geysermc.cumulus.SimpleForm;
+import org.geysermc.cumulus.response.SimpleFormResponse;
+
+public class Util {
+    public static String getLang(String key) {
+        return FloodgateCP.getInstance().getConfig().getString("lang." + key);
+    }
+    public static FileConfiguration getConfig() {
+        return FloodgateCP.getInstance().getConfig();
+    }
+    public static void handleResponse(String panel, SimpleFormResponse response, SimpleForm form, Player player) {
+
+        String buttonHandle = getConfig().getConfigurationSection("panels." + panel + ".buttons").getKeys(false).toArray()[response.getClickedButtonId()].toString();
+
+        for (String i : getConfig().getStringList("panels." + panel + ".buttons." + buttonHandle + ".commands")) {
+            if (i.startsWith("console= ")) {
+                FloodgateCP.getInstance().getServer().dispatchCommand(FloodgateCP.getInstance().getServer().getConsoleSender(), i.replaceFirst("console= ", ""));
+            } else if (i.startsWith("msg= ")) {
+                player.sendMessage(i.replaceFirst("msg= ", ""));
+            } else {
+                player.performCommand(i);
+            }
+        }
+
+    }
+}
